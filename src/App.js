9ed1d024a30bcbc055c7b71fc1410dc5f2c9b1ea
sunxinbo <sunxinbo@erwan365.com>
2024-06-16 23:13:28 +0800

    test

--- src/App.js
+++ src/App.js
@@ -2,24 +2,32 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  // const divContent = '标签内容'
+  const divTitle = '标签标题'
+
+  const flag = true 
+
   // 只能return 一个根元素
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div title={divTitle}>{divContent}</div>
+    // <div className="App">
+    //   <header className="App-header"> 
+    //     // jsx 插值写法  {}
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
