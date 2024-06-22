8d1ce6b97f5f78b5541df570b476b8f4c99bec46
sunxinbo <sunxinbo@erwan365.com>
2024-06-22 09:31:14 +0800

    父组件向子组件传值

--- src/App.js
+++ src/App.js
@@ -1,114 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
-import { Fragment, useState } from 'react';
 
-function App() {
+// function Article(prop){
+//   return (
+//     <div>
+//       <h2>{prop.title}</h2>
+//       <p>{prop.content}</p>
+//     </div>
+//   )
+// }
 
-  // const divContent = '标签内容'
-  const divTitle = '标签标题'
+function Detail({content, active}){
 
-  const flag = false 
-
-  let divContent = null
-
-  if(flag) {
-    divContent = <span>flag是true</span>
-  } else {
-    divContent = <p>flag是false</p>
-  }
-
-  const nameList = [
-    {id:1,"name":"张三"},
-    {id:2,"name":"李四"},
-    {id:3,"name":"王五"}
-  ]
-
-  
-  const ulList = nameList.map(item => (
-    // 使用 Fragment 设置多个标签
-    <Fragment key={item.id}>
-      <li>{item.name}</li>
-      <li >----------</li>
-    </Fragment>
-  ))
-
-  // 设置状态
-const [conent, setContent] = useState('标签的默认内容')
-  // 对象操作
-const [data, setData] = useState({
-  title: '默认标题',
-  content: '默认内容',
-})
-
-// 按钮事件
-  function handleClick(e) {
-    // console.log("点击按钮",e)
-    setContent('新内容')
-  }
-
-  function handleClick2(e) {
-    // console.log("点击按钮",e)
-    setData({
-      // 复制之前属性
-      ...data,
-      // 设置新标题
-      title: '新标题'
-    })
-  }
-
-  const imgData = {
-    className: 'aa',
-    style: {
-      width: 200,
-      height: 200
-    }
-  }
-
-  // 只能return 一个根元素
   return (
     <>
-        <div>{conent}</div>
-        <button onClick={handleClick}>按钮</button>
-        <div title={data.title}>{data.content}</div>
-        <button onClick={handleClick2}>按钮</button>
-        {/* <div>
-          <img
-            alt=''
-            src={logo} 
-            className='aa'
-            style={{
-              width: 200,
-              height: 200
-            }}
-          />
-        </div> */}
-        <div>
-          <img
-            src = {logo}
-            alt=''
-            {...imgData}
-          />
-        </div>
+      <p>{content}</p>
+      <p>{active ? '显示':'不显示'}</p>
     </>
-    // <ul>{ulList}</ul>
-    // <div title={divTitle}>{divContent}</div>
-    // <div className="App">
-    //   <header className="App-header"> 
-    //     // jsx 插值写法  {}
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+  )
+}
+
+// 解构
+function Article({title, detailData}){
+  return (
+    <div>
+      <h2>{title}</h2>
+      <Detail {...detailData}></Detail>
+    </div>
+  )
+}
+// 父组件向子组件传值
+function App() {
+  const articleData = {
+    title: '标题',
+    detailData : {
+      content: '内容',
+      active: true
+    }
+  }
+  return (
+    <div>
+      <Article {...articleData} ></Article>
+    </div>
   )
 }
 
---
+++ src/App2.js
@@ -0,0 +1,115 @@
+import logo from './logo.svg';
+import './App.css';
+import { Fragment, useState } from 'react';
+
+function App() {
+
+  // const divContent = '标签内容'
+  const divTitle = '标签标题'
+
+  const flag = false 
+
+  let divContent = null
+
+  if(flag) {
+    divContent = <span>flag是true</span>
+  } else {
+    divContent = <p>flag是false</p>
+  }
+
+  const nameList = [
+    {id:1,"name":"张三"},
+    {id:2,"name":"李四"},
+    {id:3,"name":"王五"}
+  ]
+
+  
+  const ulList = nameList.map(item => (
+    // 使用 Fragment 设置多个标签
+    <Fragment key={item.id}>
+      <li>{item.name}</li>
+      <li >----------</li>
+    </Fragment>
+  ))
+
+  // 设置状态
+const [conent, setContent] = useState('标签的默认内容')
+  // 对象操作
+const [data, setData] = useState({
+  title: '默认标题',
+  content: '默认内容',
+})
+
+// 按钮事件
+  function handleClick(e) {
+    // console.log("点击按钮",e)
+    setContent('新内容')
+  }
+
+  function handleClick2(e) {
+    // console.log("点击按钮",e)
+    setData({
+      // 复制之前属性
+      ...data,
+      // 设置新标题
+      title: '新标题'
+    })
+  }
+
+  const imgData = {
+    className: 'aa',
+    style: {
+      width: 200,
+      height: 200
+    }
+  }
+
+  // 只能return 一个根元素
+  return (
+    <>
+        <div>{conent}</div>
+        <button onClick={handleClick}>按钮</button>
+        <div title={data.title}>{data.content}</div>
+        <button onClick={handleClick2}>按钮</button>
+        {/* <div>
+          <img
+            alt=''
+            src={logo} 
+            className='aa'
+            style={{
+              width: 200,
+              height: 200
+            }}
+          />
+        </div> */}
+        <div>
+          <img
+            src = {logo}
+            alt=''
+            {...imgData}
+          />
+        </div>
+    </>
+    // <ul>{ulList}</ul>
+    // <div title={divTitle}>{divContent}</div>
+    // <div className="App">
+    //   <header className="App-header"> 
+    //     // jsx 插值写法  {}
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+  )
+}
+
+export default App;
